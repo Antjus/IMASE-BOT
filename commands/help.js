a86ea214cbef12d6b2ed6f24a8131dd32351a093
Antjus <76005200+Antjus@users.noreply.github.com>
2021-04-01 22:02:44 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = client => {
 
         .setTitle('Command List')
         .setColor('#CC562C')
-        .setDescription('Use: `y!help <command>` \n\n • `🛠 "moderating"` to see all the chat commands; \n\n • `📷 "images"` to see all the images commands; \n\n • `😂 "fun"` to see all the "funny" commands; \n\n • `🖱 "others"` to see...yeah, other commands.')
+        .setDescription('Use: `y!<command>` \n\n • `🛠 "moderating"` to see all the chat commands; \n\n • `📷 "images"` to see all the images commands; \n\n • `😂 "fun"` to see all the "funny" commands; \n\n • `🖱 "others"` to see...yeah, other commands.')
         .setTimestamp()
         .setFooter('IMASE', 'https://media.discordapp.net/attachments/806227461070520363/826329523103858738/08389d35b7d658e8008734e248496c61.png')
         .setThumbnail('https://images-ext-1.discordapp.net/external/QCcrCbnel2dyEUKsFcttbwUaCqU86blbXDkQaxSjxSg/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/800329693651009557/08389d35b7d658e8008734e248496c61.png?width=425&height=425')
@@ -22,7 +22,7 @@ module.exports = client => {
   client.on('message', message => { //help moderating
     if (!message.guild) return;
   
-    if (message.content.toLowerCase().startsWith(`${prefix}help moderating`)) {
+    if (message.content.toLowerCase().startsWith(`${prefix}moderating`)) {
     const embed = new MessageEmbed()
 
     .setTitle('Moderating Commands')
@@ -36,7 +36,7 @@ module.exports = client => {
   client.on('message', message => { //help images
     if (!message.guild) return;
   
-    if (message.content.toLowerCase().startsWith(`${prefix}help images`)) {
+    if (message.content.toLowerCase().startsWith(`${prefix}images`)) {
     const embed = new MessageEmbed()
 
     .setTitle('Images Commands')
@@ -50,12 +50,12 @@ module.exports = client => {
   client.on('message', message => { //help others
     if (!message.guild) return;
   
-    if (message.content.toLowerCase().startsWith(`${prefix}help others`)) {
+    if (message.content.toLowerCase().startsWith(`${prefix}others`)) {
     const embed = new MessageEmbed()
 
     .setTitle('Other Commands')
     .setColor('#CC562C')
-    .setFooter('IMASE', 'https://media.discordapp.net/attachments/806227461070520363/826329523103858738/08389d35b7d658e8008734e248496c61.png')
+    .setFooter('IMASE', 'https://media.discordapp.net/attachments/732707047984070741/826804644135174154/logo.png')
     .setDescription('Other Commands here,boi: \n\n • `y!invite` to see and use the invite link; \n\n • `y!github` to see the bot github project; \n\n • `y!testnsfw` to check if a channel is safe or not safe for work.');
   message.channel.send(embed);
     }
@@ -64,7 +64,7 @@ module.exports = client => {
   client.on('message', message => { //help fun
     if (!message.guild) return;
   
-    if (message.content.toLowerCase().startsWith(`${prefix}help fun`)) {
+    if (message.content.toLowerCase().startsWith(`${prefix}fun`)) {
     const embed = new MessageEmbed()
 
     .setTitle('Fun Commands')
