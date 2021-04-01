9dcb672260ec8cb86bf3a9626d6fd3e61d6865ed
Antjus <76005200+Antjus@users.noreply.github.com>
2021-04-01 22:02:20 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -10,7 +10,7 @@ module.exports = client => {
             .setColor('#CC562C')
             .setURL('https://github.com/Antjus/IMASE-BOT')
             .setTimestamp()
-            .setFooter('IMASE', 'https://media.discordapp.net/attachments/806227461070520363/826329523103858738/08389d35b7d658e8008734e248496c61.png')
+            .setFooter('IMASE', 'by Antus')
             .setThumbnail('https://media.discordapp.net/attachments/732707047984070741/804723382741893130/mark-github-512.png')
             .setDescription('Check my GitHub boi')
           message.channel.send(embed);
